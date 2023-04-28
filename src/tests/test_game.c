ed80f6f9d5d8ffba81f6da73c4b6ad04140de9d5
VoxLight <tkkt392@gmail.com>
2023-04-28 01:16:48 -0400

    Fixed some tests

--- src/tests/test_game.c
+++ src/tests/test_game.c
@@ -17,8 +17,8 @@ void test_init_game() {
     assert(game->is_playing == false);
     assert(game->num_players == 0);
     assert(game->players != NULL);
-    assert(game->turn_order.head == NULL);
-    assert(game->turn_order.tail == NULL);
+    assert(game->turn_order->head == NULL);
+    assert(game->turn_order->tail == NULL);
     assert(game->current_player_node == NULL);
 }
 
@@ -29,10 +29,10 @@ void test_game_add_player() {
     Game* game = get_current_game();
     assert(game->num_players == 1);
     assert(hash_map_get(game->players, "Alice") != NULL);
-    assert(game->turn_order.head != NULL);
-    assert(game->turn_order.tail != NULL);
-    assert(game->turn_order.head->data == player);
-    assert(game->turn_order.tail->data == player);
+    assert(game->turn_order->head != NULL);
+    assert(game->turn_order->tail != NULL);
+    assert(game->turn_order->head->data == player);
+    assert(game->turn_order->tail->data == player);
 }
 
 void test_start_game() {
@@ -41,7 +41,7 @@ void test_start_game() {
     Game* game = get_current_game();
     assert(game->is_playing == true);
     assert(game->current_player_node != NULL);
-    assert(game->current_player_node->data == game->turn_order.head->data);
+    assert(game->current_player_node->data == game->turn_order->head->data);
 }
 
 // TODO: Implement other test functions for get_card_owner, get_target_card, etc.
